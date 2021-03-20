92311301109ca41f6a44f33cba7f8f818a802ce8
unknown <vipulawasthi11@gmail.com>
2021-03-20 17:31:12 +0530

    into

---
+++ array/init.java
@@ -0,0 +1,16 @@
+class st 
+  {
+    public static void cal()
+    {
+        int i=1;
+        int j=1;
+        for(i=1;i<=9;i++)
+        {
+            for(j=i;j<=9;j++)
+            {
+                
+            System.out.print("*");
+        }
+        System.out.println();
+        }}
+    }
